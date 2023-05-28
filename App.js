8002d123bf6ea13af1ab6a553b51a04f5a146c12
phomearadev <phomearadev@gmail.com>
2023-05-28 08:53:11 +0100

    first update

--- App.js
+++ App.js
@@ -1,27 +1,42 @@
+
+
+
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
 
 const Flex = () => {
   return (
     <View
       style={[
-        styles.container,
+        styles.container1,
         {
           // Try setting `flexDirection` to `"row"`.
-          flexDirection: 'column',
+
+          flexDirection: 'row',
         },
       ]}>
-      <View style={{flex: 1, backgroundColor: 'red'}} />
-      <View style={{flex: 2, backgroundColor: 'darkorange'}} />
-      <View style={{flex: 3, backgroundColor: 'green'}} />
+      <View style={{flex: 1, backgroundColor: 'orange'}} />
+      <Text style={{ backgroundColor: 'orange'}}>  29/05/23  </Text>
+      <View>
+      <View style={{flex: 1, backgroundColor: 'darkorange'}}/>
+      <Text style={{ backgroundColor: 'darkorange'}}>  Wouldn't it be great if?  </Text>
+      </View>
     </View>
+    
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  container1: {
+    marginTop:50,
     flex: 1,
-    padding: 20,
+    padding: 10,
+  },
+  container2: {
+    flex: 1,
+    padding: 0,
   },
 });
 
