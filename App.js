41463a795ecba222f2689120b9b7419691f3e750
phomearadev <phomearadev@gmail.com>
2023-07-04 00:11:47 +0100

    Version1Working!

--- App.js
+++ App.js
@@ -1,31 +1,116 @@
 
 
 
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, {useState}  from 'react';
+import {StyleSheet, Text, TextInput, View} from 'react-native';
 
-const Flex = () => {
+// <View style={{flex: 3, backgroundColor: 'green'}} />
+
+
+
+  
+  
+
+const MyFlex = () => {
+
+  const [text, setText] = useState('');
+
   return (
-    <View
-      style={[
-        styles.container,
-        {
-          // Try setting `flexDirection` to `"row"`.
-          flexDirection: 'column',
-        },
-      ]}>
-      <View style={{flex: 2, backgroundColor: 'red'}} />
-      <View style={{flex: 2, backgroundColor: 'darkorange'}} />
-      <View style={{flex: 3, backgroundColor: 'green'}} />
+ <>
+   
+   <View style={[styles.container0, {flexDirection: 'column',},]}>
+
     </View>
+    <View style={[styles.container1, {flexDirection: 'row',},]}>
+      
+      <View>
+      <TextInput style={{flex: 2, backgroundColor: 'darkorange', padding: 5, fontSize:15 }}
+        placeholder="Enter Date!"
+        onChangeText={newText => setText(newText)}
+        defaultValue={''}
+      />
+      
+    </View>
+      <View style={{flex: 3, borderLeftWidth:5, backgroundColor: 'darkorange'}}>
+
+      <Text style={styles.baseText}>
+      <Text style={styles.titleText}>
+      Wouldn't It Be Great If?
+      </Text>
+      </Text>
+      </View>
+     
+      </View>
+      <View style={[styles.container2, {flexDirection: 'column',},]}>
+      <View style={{flex: 1, backgroundColor: 'darkorange'}} />
+      <TextInput style={{flex: 3, backgroundColor: 'darkorange', padding: 5, fontSize:30, fontStyle:"italic", textAlign:"center", }}
+        placeholder="Enter WIBGI Initiative!"
+        onChangeText={newText => setText(newText)}
+        defaultValue={''}
+      />
+      <View style={{flex: 2, backgroundColor: 'darkorange'}} />
+      
+  
+      </View>
+      <View style={[styles.container0, {flexDirection: 'column',},]}>
+
+      </View>
+    
+      
+    </>
+
+
+
+
+  
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  container0:{
+    flex: 1.5,
+    backgroundColor: 'slategrey'    
+  },
+  container1: {
+    //marginTop: 70,
     flex: 1,
-    padding: 20,
+    padding: 0,
+    //paddingBottom: 50,
+  borderTopWidth:3,
+  borderTopColor:"darkgrey",
+  borderBottomWidth: 5,
+  borderLeftWidth:3,
+  borderLeftColor:"darkgrey",
+  borderRightWidth:3,
+  borderRightColor:"darkgrey",
+  },
+  container2: {
+    paddingTop: 0,
+    flex: 2,
+    padding: 0,
+    paddingBottom: 0,
+    borderBottomWidth: 3,
+    borderBottomColor:"darkgrey",
+    borderLeftWidth:3,
+    borderLeftColor:"darkgrey",
+    borderRightWidth:3,
+    borderRightColor:"darkgrey",
+  
+  },
+
+  baseText: {
+    fontFamily: 'Cochin',
+    textAlign:"center",
+    marginTop:32,
+    paddingBottom: 0,
+    padding: 0,
+
+  },
+  titleText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    
   },
 });
 
-export default Flex;
+export default MyFlex;
