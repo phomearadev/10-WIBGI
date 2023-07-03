2a6c8bc81eceb7f6e4b13b058a0ff341f19c4e6a
phomearadev <phomearadev@gmail.com>
2023-07-03 21:26:37 +0100

    nowthis

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ const Flex = () => {
         },
       ]}>
       <View style={{flex: 2, backgroundColor: 'red'}} />
-      <View style={{flex: 1, backgroundColor: 'darkorange'}} />
+      <View style={{flex: 2, backgroundColor: 'darkorange'}} />
       <View style={{flex: 3, backgroundColor: 'green'}} />
     </View>
   );
